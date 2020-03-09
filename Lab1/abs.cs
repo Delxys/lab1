b8714786ca607a915103bbb36f104e01f0ab2ffc
vitasssss753 <vitasssss753@gmail.com>
2020-03-09 10:45:12 +0300

    test

---
+++ Lab1/abs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    abstract class abs
+    {
+        public abs() 
+        { }
+        public double a { get;}
+        public double b { get;}
+    }
+
+}
